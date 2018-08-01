f8273d9f81e56ad7f4608f7f9501914eb49ae8dd
HoffsMH <hecker.matthew@gmail.com>
2018-07-31 23:15:54 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Joken.Signer.Config.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def description do
     "A simple configuration plugin for Joken Signers"
